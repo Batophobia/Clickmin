ed5a30acd752121ae5bff9f206b51b7a9b240bee
Batophobia <godrox3@yahoo.com>
2014-08-08 17:31:33 -0500

    Startup
    Nothing much yet, but soon

---
+++ js/items.js
@@ -0,0 +1,5 @@
+items:{
+	nectar:{total:0,	owned:0	},
+	bombRock:{total:0,	owned:0	},
+	pellets:[]  //color, numNeeded, numRed, numYellow, numBlue, x, y
+};
---
+++ js/pikmin.js
@@ -0,0 +1,75 @@
+var pikmin = {
+	party:{
+		total:0,
+		red:{
+			display: "Red",
+			canBomb: false,
+			strength: 15,
+			speed: 2,
+			carry: 1,
+			numLeaf: 0,
+			numbud: 0,
+			numflower: 0
+		},
+		yellow:{
+			display: "Yellow",
+			canBomb:true,
+			strength: 10,
+			speed: 2,
+			carry: 1,
+			numLeaf:0,
+			numbud:0,
+			numflower:0
+		},
+		blue:{
+			display: "Blue",
+			canBomb:false,
+			strength: 10,
+			speed: 2,
+			carry: 1,
+			numLeaf:0,
+			numbud:0,
+			numflower:0
+		},
+		purple:{
+			display: "Purple",
+			canBomb:false,
+			strength: 20,
+			speed: 1,
+			carry: 10,
+			numLeaf:0,
+			numbud:0,
+			numflower:0
+		},
+		white:{
+			display: "White",
+			canBomb:false,
+			strength: 10,
+			speed: 3,
+			carry: 1,
+			numLeaf:0,
+			numbud:0,
+			numflower:0
+		},
+		rock:{
+			display: "Rock",
+			canBomb:false,
+			strength: 15,
+			speed: 2,
+			carry: 1,
+			numLeaf:0,
+			numbud:0,
+			numflower:0
+		},
+		pink:{
+			display: "Winged",
+			canBomb:false,
+			strength: 7,
+			speed: 2,
+			carry: 1,
+			numLeaf:0,
+			numbud:0,
+			numflower:0
+		}
+	}
+};
